c911112c0549c4a7da180ef85e29317d3f0df684
root <root@ip-10-42-225-60.ec2.internal>
2026-07-16 14:35:02 +0000

    print lower alphabet except for q e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
