87b7bb251b6d5a80c3f8e4d6884631888400adda
root <root@ip-10-42-225-60.ec2.internal>
2026-07-16 16:56:19 +0000

    prints alphabet lowercase no return

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
