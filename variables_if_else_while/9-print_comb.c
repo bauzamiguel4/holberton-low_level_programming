b54a0b0c51cb1580fa75c27e3f5583fa634e0b22
root <root@ip-10-42-225-60.ec2.internal>
2026-07-16 16:13:00 +0000

    all posible combos silgle digits

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	putchar(a + '0');
+	if (a < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
