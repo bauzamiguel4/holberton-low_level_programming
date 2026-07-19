8bd07f4c70a4f1551785033b402aa8ca7308c468
root <root@ip-10-42-21-87.ec2.internal>
2026-07-19 15:58:33 +0000

    n - 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ * @n: start num
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("%d\n", 98);
+}
