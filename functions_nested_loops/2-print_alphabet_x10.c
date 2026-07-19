cdfd83b611b790239d23014c1072e1b33fffa99d
root <root@ip-10-42-151-191.ec2.internal>
2026-07-19 05:26:57 +0000

    added printn alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	i = 0;
+	while (i < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
