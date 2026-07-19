7ca28a7adcc7d60a34d7b2104eb0518728638a42
root <root@ip-10-42-21-87.ec2.internal>
2026-07-19 15:44:44 +0000

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - gets abs value
+ * @n: num
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
