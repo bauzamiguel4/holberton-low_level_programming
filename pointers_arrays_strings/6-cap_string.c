317aba2eba71678f398e8d4382e911866519dc33
root <root@ip-10-42-34-177.ec2.internal>
2026-07-27 01:47:18 +0000

    changed file

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,36 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters to uppercase
- * @s: target string
+ * cap_string - capitalizes all words of a string
+ * @s: string to capitalize
  *
- * Return: pointer to modified string
+ * Return: pointer to s
  */
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
 	int i;
+	int j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
 	i = 0;
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] -= 32;
+	}
+
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		j = 0;
+		while (sep[j] != '\0')
 		{
-			s[i] = s[i] - 32;
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+			j++;
 		}
 		i++;
 	}
