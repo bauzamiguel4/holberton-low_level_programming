f93a27fd0fdf9e2ef2f1f72ae11fcf72c2c96447
root <root@ip-10-42-183-121.ec2.internal>
2026-08-03 01:31:31 +0000

    bulk recursion tasks

---
+++ pointers_arrays_strings/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * actual_prime - helper function to find if a number is prime
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+
+	return (actual_prime(n, i - 1));
+}
+
+/**
+ * is_prime_number - checks if an integer is a prime number
+ * @n: number to evaluate
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	return (actual_prime(n, n - 1));
+}
